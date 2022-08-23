f99714d7d9aa2aff8b7c30cb5ec51729af323619
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-08-23 12:07:51 +0300

    clock

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(function () {
+	const clock = document.querySelector(".display");
+	let time = new Date();
+	let sec = time.getSeconds();
+	let min = time.getMinutes();
+	let hr = time.getHours();
+	if (sec < 10) {
+		sec = `0${sec}`;
+	}
+	if (min < 10) {
+		min = `0${min}`;
+	}
+
+	clock.textContent = `${hr}:${min}:${sec}`;
+});
